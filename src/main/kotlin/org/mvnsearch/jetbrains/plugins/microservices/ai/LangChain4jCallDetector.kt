3ecf81a303f510bbe5c57b9d87b6116f972e444c
Libing Chen <libing.chen@gmail.com>
2025-05-03 13:22:35 +0800

    app: LangChain4jCallDetector

---
+++ src/main/kotlin/org/mvnsearch/jetbrains/plugins/microservices/ai/LangChain4jCallDetector.kt
@@ -0,0 +1,54 @@
+package org.mvnsearch.jetbrains.plugins.microservices.ai
+
+import com.intellij.codeInsight.AnnotationUtil
+import com.intellij.openapi.project.Project
+import org.jetbrains.uast.UCallExpression
+
+import org.strangeway.msa.db.InteractionType
+import org.strangeway.msa.frameworks.CallDetector
+import org.strangeway.msa.frameworks.FrameworkInteraction
+import org.strangeway.msa.frameworks.Interaction
+import org.strangeway.msa.frameworks.hasLibraryClass
+
+class LangChain4jCallDetector : CallDetector {
+    private val interaction: Interaction = FrameworkInteraction(InteractionType.REQUEST, "LangChain4j")
+    private val aiMessageAnnotations = listOf(
+        "dev.langchain4j.service.SystemMessage",
+        "dev.langchain4j.service.UserMessage"
+    )
+    private val aiServiceAnnotations =
+        listOf("dev.langchain4j.service.spring.AiService", "io.quarkiverse.langchain4j.RegisterAiService")
+    private val langChainStubInterfaces =
+        listOf("dev.langchain4j.model.chat.ChatModel", "dev.langchain4j.model.chat.StreamingChatModel")
+    private val langChainStubMethods = listOf("chat")
+    override fun getCallInteraction(project: Project, uCall: UCallExpression): Interaction? {
+        val psiMethod = uCall.resolve()
+        if (psiMethod != null) {
+            if (AnnotationUtil.isAnnotated(psiMethod, aiMessageAnnotations, 0)) {
+                return interaction
+            } else {
+                val psiClass = psiMethod.containingClass
+                if (psiClass != null) {
+                    val psiClassFullName = psiClass.qualifiedName!!
+                    if (psiClass.isInterface && AnnotationUtil.isAnnotated(psiClass, aiServiceAnnotations, 0)) {
+                        return interaction
+                    } else if (isLangChainServiceStub(psiClassFullName)) {
+                        if (langChainStubMethods.contains(psiMethod.name)) {
+                            return interaction
+                        }
+                    }
+                }
+            }
+        }
+        return null
+    }
+
+    override fun isAvailable(project: Project): Boolean {
+        return hasLibraryClass(project, "dev.langchain4j.model.chat.ChatModel")
+    }
+
+    private fun isLangChainServiceStub(classFullName: String): Boolean {
+        return langChainStubInterfaces.contains(classFullName)
+    }
+}
+
